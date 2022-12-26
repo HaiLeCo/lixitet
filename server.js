947100aa499735e83a6523b7dfb1d2719404e593
lhlhai <lhlhai.spkt@gmail.com>
2022-12-26 19:51:43 +1100

    Update server.js

--- server.js
+++ server.js
@@ -300,7 +300,7 @@ dbo.connectToServer(function (err) {
     process.exit();
   }
 
-  const PORT = process.env.PORT | 8080;
+  const PORT = process.env.PORT || 3001;
   app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
   });
